eb9257cdfb03772b4907d44f50f09fa39836e41c
oswaldburgos1 <70352221+oswaldburgos1@users.noreply.github.com>
2020-10-01 10:19:19 -0400

    Add files via upload

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Instagram Demo</h1>
+      <h2>Your Pictures</h2>
+      <p>Welcome to my app, feel free to upload a picture!</p>
+    </div>
+  )
+}
+
+export default Title;
